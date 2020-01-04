22b5f5941a2f1ff308c40dd6896a86ed6dedf030
Damian Cupo <as2fan@gmail.com>
2020-01-04 01:20:35 -0300

    Initial commit of the test files

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using static SFMLTest.RayCaster;
+
+namespace SFMLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySFMLProgram app = new MySFMLProgram();
+            app.StartSFMLProgram();
+        }
+
+
+    }
+
+    class MySFMLProgram
+    {
+        RenderWindow window;
+        Font font;
+        RayCaster caster;
+
+        Vector2i screen = new Vector2i(100,75);
+        Vector2f pixel;
+
+        float fov = 90;
+        float distanceProjectionPlane;
+
+        int[,] _m = {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,0,0,0,0,1,1,0,1,1,1,0,1,1},
+        {1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1},
+        {1,0,1,0,0,1,1,1,1,0,1,0,0,0,0,1},
+        {1,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+        };
+
+
+        public void Render(Vector2f player, float angle)
+        {
+            List<Vertex> vertices = new List<Vertex>();
+
+            for (int x = 0; x < screen.X;x++)
+            {
+                float rayAngle = AtanD((x - screen.X / 2.0f) / distanceProjectionPlane);
+                RayResult ray = caster.RayCast(player, angle + rayAngle);
+                float lineHeightHalf = ((caster.CellSize / (ray.Magnitude * CosD(rayAngle))) * distanceProjectionPlane)/2;
+                Color color = Color.Black;
+                switch (ray.Side)
+                {
+                    case Side.Down:
+                    case Side.Up:
+                        color = new Color(255, 255, 255, 255);
+                        break;
+                    case Side.Left:
+                    case Side.Right:
+                        color = new Color(128, 128, 128, 255);
+                        break;
+                }
+
+                vertices.Add(new Vertex
+                    {
+                        Position = new Vector2f(x * pixel.X, Round(screen.Y / 2 - lineHeightHalf) * pixel.Y),
+                        Color = color
+                    });
+                vertices.Add(new Vertex
+                {
+                    Position = new Vector2f((x + 1) * pixel.X, Round(screen.Y / 2 - lineHeightHalf) * pixel.Y),
+                    Color = color
+                });
+                vertices.Add(new Vertex
+                {
+                    Position = new Vector2f((x + 1) * pixel.X, Round(screen.Y / 2 + lineHeightHalf) * pixel.Y),
+                    Color = color
+                });
+                vertices.Add(new Vertex
+                {
+                    Position = new Vector2f(x * pixel.X, Round(screen.Y / 2 + lineHeightHalf) * pixel.Y),
+                    Color = color
+                });
+            }
+
+            window.Draw(vertices.ToArray(), 0, (uint)vertices.Count, PrimitiveType.Quads);
+        }
+
+        public void StartSFMLProgram()
+        {
+            #region Inicialization
+            bool[,] Map = new bool[_m.GetLength(0), _m.GetLength(1)];
+
+            for (int x = 0; x < _m.GetLength(0); x++)
+                for (int y = 0; y < _m.GetLength(1); y++)
+                    Map[x, y] = _m[x, y] == 0 ? false : true;
+
+            window = new RenderWindow(new VideoMode(800, 600), "SFML window");
+            window.SetVisible(true);
+            window.Closed += new EventHandler(OnClosed);
+            window.KeyPressed += new EventHandler<KeyEventArgs>(OnKeyPressed);
+
+            pixel = new Vector2f(window.Size.X / screen.X,window.Size.Y / screen.Y);
+            distanceProjectionPlane = (screen.X / 2) / TanD(fov / 2);
+            caster = new RayCaster
+            {
+                CellSize = 16,
+                Map = Map
+            };
+            #endregion
+
+            Vector2f player = new Vector2f(80,80);
+            float angle = 0;
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Black);
+
+                Render(player, angle++);
+
+                window.Display();
+                Thread.Sleep(10);
+            }
+        }
+
+        #region Event listeners
+        void OnClosed(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+
+        void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+            if (e.Code == Keyboard.Key.Escape)
+                window.Close();
+        } 
+        #endregion
+    }
+}
---
+++ RayCaster.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.System;
+
+namespace SFMLTest
+{
+    class RayCaster
+    {
+        #region Constants
+        const float DegRad = 3.14159265359f / 180.0f;
+        const float MaxDistance = 60000f;
+        #endregion
+
+        #region Properties
+        public float CellSize { get; set; }
+
+        public bool[,] Map { get; set; } 
+        #endregion
+
+        #region Clases
+        public enum Side
+        {
+            Up = 0,
+            Right,
+            Down,
+            Left
+        }
+
+        public class RayResult
+        {
+            public Vector2f Position { get; set; }
+            public float Magnitude { get; set; }
+            public Side Side { get; set; }
+        } 
+        #endregion
+
+        private bool GetMap(int px, int py)
+        {
+            if (px < 0 || px > Map.GetLength(0) - 1 || py < 0 || py > Map.GetLength(1) - 1)
+                return true;
+            else
+                return Map[px, py];
+        }
+
+        public static float TanD(float A) => (float)Math.Tan(A*DegRad);
+        public static float CosD(float A) => (float)Math.Cos(A*DegRad);
+        public static float SinD(float A) => (float)Math.Sin(A*DegRad);
+        public static float AtanD(float L) => (float)Math.Atan(L)/DegRad;
+        public static int Round(float N) => (int)Math.Floor(N);
+
+        public RayResult RayCast(Vector2f O, float A)
+        {
+            Vector2f D = O + new Vector2f(CosD(A) * 100, SinD(A) * 100);
+            Vector2f Slope = D - O;
+            Vector2f Delta = new Vector2f();
+            Vector2f Ph = new Vector2f(), Pv = new Vector2f();
+            float Dh, Dv;
+            RayResult res;
+
+            //If Dx is zero, then there's no horizontal intersections
+            if (Slope.X == 0)
+            {
+                Ph = new Vector2f(O.X + MaxDistance, O.Y + MaxDistance);
+                Dh = MaxDistance;
+            }
+            else
+            {
+                Delta.X = CellSize * Math.Sign(Slope.X);
+
+                Ph.X = (float)Math.Floor(O.X / CellSize);
+                Ph.X = (Ph.X + (Math.Sign(Delta.X) == 1 ? 1 : 0)) * CellSize;
+
+                if (Slope.Y == 0)
+                {
+                    Delta.Y = 0;
+                    Ph.Y = O.Y;
+                }
+                else
+                {
+                    Delta.Y = (Delta.X * Slope.Y) / Slope.X;
+                    Ph.Y = O.Y + (Slope.Y * (Ph.X - O.X)) / Slope.X;
+                }
+
+                while (!GetMap((int)Math.Floor(Ph.X / CellSize) + (Delta.X < 0 ? -1 : 0), (int)Math.Floor(Ph.Y / CellSize)))
+                    Ph += Delta;
+
+                Dh = (float)Math.Sqrt(Math.Pow((Ph.X - O.X), 2) + Math.Pow((Ph.Y - O.Y), 2));
+            }
+
+            //If Dy is zero, then there's no vertical intersections
+            if (Slope.Y == 0)
+            {
+                Pv = new Vector2f(O.X + MaxDistance, O.Y + MaxDistance);
+                Dv = MaxDistance;
+            }
+            else
+            {
+                Delta.Y = CellSize * Math.Sign(Slope.Y);
+
+                Pv.Y = (float)Math.Floor(O.Y / CellSize);
+                Pv.Y = (Pv.Y + (Math.Sign(Delta.Y) == 1 ? 1 : 0)) * CellSize;
+
+                if (Slope.X == 0)
+                {
+                    Delta.X = 0;
+                    Pv.X = O.X;
+                }
+                else
+                {
+                    Delta.X = (Delta.Y * Slope.X) / Slope.Y;
+                    Pv.X = O.X + (Slope.X * (Pv.Y - O.Y)) / Slope.Y;
+                }
+
+
+                while (!GetMap((int)Math.Floor(Pv.X / CellSize), (int)Math.Floor(Pv.Y / CellSize) + (Delta.Y <0 ? -1 : 0)))
+                    Pv += Delta;
+
+                Dv = (float)Math.Sqrt(Math.Pow(Pv.X - O.X, 2) + Math.Pow(Pv.Y - O.Y, 2));
+            }
+
+
+            if (Dh < Dv)
+            {
+                res = new RayResult
+                {
+                    Position = Ph,
+                    Magnitude = Dh,
+                    Side = Slope.X < 0 ? Side.Left : Side.Right
+                };
+
+                return res;
+            }
+            else
+            {
+                res = new RayResult
+                {
+                    Position = Pv,
+                    Magnitude = Dv,
+                    Side = Slope.Y < 0 ? Side.Up : Side.Down
+                };
+
+                return res;
+            }
+        }
+
+    }
+}
